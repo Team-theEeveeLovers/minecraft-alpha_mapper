80d5a4c6263c0f2e6fcc08136c1e2609ef1e79cc
Sean O'Neal <sdubs08@gmail.com>
2024-08-03 00:43:26 -0400

    Move macro redefinition for fix to new include

--- MC-AlphaMapper/Main.h
+++ MC-AlphaMapper/Main.h
@@ -1,9 +1,6 @@
 #pragma once
 
 #include <iostream>
-// trickery to get around unresolved external errors caused by a "lack" of main function
-// the macro by SDL to expand main to SDL_main breaks ninja
-#define main main
 
 
 bool initMain();
--- MC-AlphaMapper/globals.h
+++ MC-AlphaMapper/globals.h
@@ -33,3 +33,6 @@
 #endif
 
 #include <SDL2/SDL.h>
+// trickery to get around unresolved external errors caused by a "lack" of main function
+// the macro by SDL to expand main to SDL_main breaks ninja
+#define main main

cb962dc50dff457fd077fca2ff79504ba5f8685c
Sean O'Neal <sdubs08@gmail.com>
2024-08-03 02:18:05 -0400

    Add renderer abstraction

---
+++ MC-AlphaMapper/Renderer.cpp
@@ -0,0 +1,75 @@
+#include "Renderer.hpp"
+
+/**
+* @file Renderer.cpp
+* @brief The source file for the render handler of the TtEL Minecraft Alpha Level Mapper
+* @author Team theEeveeLovers Sean
+* @date August 2nd, 2024
+*/
+
+
+
+bool RENDERER::checkRenderer()
+{
+	if (SDLR == NULL)
+		return false;
+	else
+		return true;
+}
+
+SDL_Renderer* RENDERER::getRenderer()
+{
+	return SDLR;
+}
+
+bool RENDERER::createRenderer(SDL_Window* window, SDL_RendererFlags flags, int index)
+{
+	SDLR = SDL_CreateRenderer(window, index, flags);
+	if (window == NULL) {
+		ASSERT(window != NULL && "Tried to create a renderer from an invalid window!");
+		return false;
+	}
+	else if (SDLR == NULL) {
+		SDL_LogError(0, "SDL renderer creation failed. SDL Error: %s\n", SDL_GetError());
+		ASSERT(SDLR != NULL && "Creating SDL renderer failed.");
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+void RENDERER::setDrawColor(Uint8 red, Uint8 green, Uint8 blue, Uint8 alpha)
+{
+	if (!checkRenderer()) {
+		ASSERT(SDLR != NULL && "Tried to set draw color of an invalid renderer!");
+	}
+	else {
+		SDL_SetRenderDrawColor(SDLR, red, green, blue, alpha);
+	}
+}
+
+void RENDERER::renderClear()
+{
+	if (!checkRenderer()) {
+		ASSERT(SDLR != NULL && "Tried to render using an invalid renderer!");
+	}
+	else {
+		SDL_RenderClear(SDLR);
+	}
+}
+
+void RENDERER::renderPresent()
+{
+	if (!checkRenderer()) {
+		ASSERT(SDLR != NULL && "Tried to render using an invalid renderer!");
+	}
+	else {
+		SDL_RenderPresent(SDLR);
+	}
+}
+
+void RENDERER::destroyRenderer()
+{
+	SDL_DestroyRenderer(SDLR);
+}
---
+++ MC-AlphaMapper/Renderer.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+
+
+#include "globals.h"
+
+/**
+* @file Renderer.hpp
+* @brief The header file for the render handler of the TtEL Minecraft Alpha Level Mapper
+* @author Team theEeveeLovers Sean
+* @date August 2nd, 2024
+*/
+
+class RENDERER {
+private:
+	// INTERNAL FUNCTION: check if the current render is valid, if not, returns false
+	bool checkRenderer();
+public:
+	// Get the SDL_Renderer* context contained within
+	SDL_Renderer* getRenderer();
+
+	/**
+	 * @brief Creates a rendering context.
+	 * @param window - the window to attach the rendering context to
+	 * @param flags - flags to create the renderer with, defaults to creating a software renderer
+	 * @param index - the index of the rendering device to create the context with, or leave blank for the first supported device
+	 * @return true if renderer created successfully, or false if error occurred.
+	 */
+	bool createRenderer(SDL_Window* window, SDL_RendererFlags flags = SDL_RENDERER_SOFTWARE, int index = -1);
+
+	/**
+	 * @brief Sets the drawing color of the renderer
+	 * @param red - the red component of the draw color
+	 * @param green - the green component of the draw color
+	 * @param blue - the blue component of the draw color
+	 * @param alpha - the alpha component of the draw color, where 0 is fully transparent, and 255 is fully opaque. Leave blank for fully opaque.
+	 */
+	void setDrawColor(Uint8 red, Uint8 green, Uint8 blue, Uint8 alpha = SDL_ALPHA_OPAQUE);
+
+	// clears the backbuffer of the renderer using the draw color, starting a new frame
+	void renderClear();
+
+	// renders everything that is currently buffered
+	void renderPresent();
+
+
+	// destroys the rendering context and frees memory
+	void destroyRenderer();
+protected:
+	// The main rendering context
+	SDL_Renderer* SDLR = NULL;
+
+};

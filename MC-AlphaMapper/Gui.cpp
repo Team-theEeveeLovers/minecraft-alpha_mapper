8e14f6578b9f8362c963bc2cdce5c3fc56abfff2
Sean O'Neal <sdubs08@gmail.com>
2024-08-06 12:21:21 -0400

    Make ImGui handler library

---
+++ MC-AlphaMapper/Gui.cpp
@@ -0,0 +1,94 @@
+#include "Gui.hpp"
+
+
+/**
+* @file Gui.cpp
+* @brief The source file for the ImGui handler of the TtEL Minecraft Alpha Level Mapper
+* @author Team theEeveeLovers Sean
+* @date August 6th, 2024
+*/
+
+bool ImGui_CONTEXT::createContext(void)
+{
+	GUI_CONTEXT = ImGui::CreateContext();
+	if (!GUI_CONTEXT->Initialized) {
+		return false;
+		initalized = false;
+	}
+	else {
+		return true;
+		initalized = true;
+	}
+	backend_init = false;
+}
+
+bool ImGui_CONTEXT::initalizeForSDL2(WINDOW window, RENDERER renderer)
+{
+	backend_init = false;
+
+	if (!initalized) 
+		ASSERT(initalized && "Tried to initalize ImGui backend without initalizing ImGui first!");
+	else if (!ImGui_ImplSDL2_InitForSDLRenderer(window, renderer)) 
+		SDL_LogError(0, "Failed to initalize ImGui for SDL2.\n\n");
+	else if (!ImGui_ImplSDLRenderer2_Init(renderer)) 
+		SDL_LogError(0, "Failed to initalize ImGui for SDL_Renderer.\n\n");
+	else {
+		backend_init = true;
+		initalizedRenderer = renderer;
+	}
+
+	return backend_init;
+}
+
+bool ImGui_CONTEXT::setCurrentContext()
+{
+	if (!initalized) {
+		return false;
+		ASSERT(initalized && "Tried to set the current ImGui context used by ImGui without initalizing ImGui!");
+	}
+	else {
+		ImGui::SetCurrentContext(GUI_CONTEXT);
+		return true;
+	}
+}
+
+void ImGui_CONTEXT::newFrame(void)
+{
+	if (!backend_init)
+		ASSERT(backend_init && "Tried to start a new ImGui frame without initalizing the render backend!");
+	else if (!initalized)
+		ASSERT(initalized && "Tried to start a new ImGui frame without initalizing ImGui!");
+	else {
+		ImGui_ImplSDLRenderer2_NewFrame();
+		ImGui_ImplSDL2_NewFrame();
+		ImGui::NewFrame();
+	}
+}
+
+void ImGui_CONTEXT::renderPresent(void)
+{
+	if (!backend_init)
+		ASSERT(backend_init && "Tried to render an ImGui frame without initalizing the render backend!");
+	else if (!initalized)
+		ASSERT(initalized && "Tried to render an ImGui frame without initalizing ImGui!");
+	else {
+		ImGui::Render();
+		ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), initalizedRenderer);
+	}
+}
+
+void ImGui_CONTEXT::shutdownContext(void)
+{
+	ImGui_ImplSDL2_Shutdown(); // shut down backend
+	if (initalized)
+		ImGui::DestroyContext(GUI_CONTEXT); // destroy context
+
+	// free pointers
+	GUI_CONTEXT = nullptr;
+	initalizedRenderer = NULL;
+
+	// reset bools
+	initalized = false;
+	backend_init = false;
+	
+}
---
+++ MC-AlphaMapper/Gui.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#include "globals.h"
+#include "Window.hpp"
+#include "Renderer.hpp"
+
+// include imgui_internal in order to get access to variables in ImGuiContext* structures
+#include <imgui_internal.h>
+
+/**
+* @file Gui.hpp
+* @brief The header file for the ImGui handler of the TtEL Minecraft Alpha Level Mapper
+* @author Team theEeveeLovers Sean
+* @date August 6th, 2024
+*/
+
+
+class ImGui_CONTEXT {
+public:
+	/**
+	 * @brief Creates the ImGui context 
+	 * @return True on success, or false on failure
+	 */
+	bool createContext(void);
+
+	/**
+	 * @brief Initalizes the context
+	 * @param window - WINDOW type to initalize for
+	 * @param renderer - RENDERER type to initalize for
+	 * @return True on success, or false on failure
+	 */
+	bool initalizeForSDL2(WINDOW window, RENDERER renderer);
+
+	/**
+	 * @brief Sets this context as the current one used by ImGui.
+	 * If you only have one context, you do not need to call this function.
+	 * @return True on success, or false on failure
+	 */
+	bool setCurrentContext();
+
+	/**
+	 * @brief Starts a new frame on the current context used by ImGui. Run this *after* you run renderClear() on your renderer.
+	 */
+	void newFrame(void);
+
+
+	/**
+	 * @brief Renders everything that is currently buffered on the current context used by ImGui. Run this **before** you run renderPresent() on your renderer.
+	 */
+	void renderPresent(void);
+
+	/**
+	 * @brief Shuts down the context and frees memory
+	 */
+	void shutdownContext(void);
+
+protected:
+	// the main context structure
+	ImGuiContext* GUI_CONTEXT = NULL;
+
+	// is the context initalized?
+	bool initalized = false;
+
+	// is the SDL2 backend initalized?
+	bool backend_init = false;
+
+	// the SDL_Renderer* the context is initalized with, if any.
+	SDL_Renderer* initalizedRenderer = NULL;
+};

0a05036702c0a0c796a34781b9c5b47ac8c100ea
Sean O'Neal <sdubs08@gmail.com>
2024-08-07 10:41:24 -0400

    Add basic fileloading code Now to add file menu to actually test it...

--- MC-AlphaMapper/FileLoading.cpp
+++ MC-AlphaMapper/FileLoading.cpp
@@ -6,3 +6,34 @@
 * @author Team theEeveeLovers Sean
 * @date August 6th, 2024
 */
+
+bool LEVEL_DATA::loadFile(std::string path)
+{
+	RWops = SDL_RWFromFile(path.c_str(), "r+");
+	if (RWops == NULL) {
+		SDL_LogError(0, "SDL could not open the file '%s'! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		return false;
+	}
+	else {
+		SDL_RWread(RWops, &compressedData, 1, SDL_RWsize(RWops));
+		const char* compressed_pointer = compressedData.data();
+		std::string decompressed_data = gzip::decompress(compressed_pointer, compressedData.size());
+		SDL_RWclose(RWops); // close the stream for right now as we don't need to write anything yet
+
+		std::string outPath = path.append("_temp");
+		out_RWops = SDL_RWFromFile(outPath.c_str(), "w+");
+		if (out_RWops == NULL) {
+			SDL_LogError(0, "SDL could not make new temp file '%s'! SDL Error: %s\n", outPath.c_str(), SDL_GetError());
+			return false;
+		}
+		else {
+			SDL_RWwrite(out_RWops, decompressed_data.data(), decompressed_data.size(), 1);
+		}
+	}
+}
+
+void LEVEL_DATA::closeFile(void)
+{
+	if (RWops != NULL)
+		SDL_RWclose(RWops);
+}
--- MC-AlphaMapper/FileLoading.hpp
+++ MC-AlphaMapper/FileLoading.hpp
@@ -2,8 +2,6 @@
 #include "globals.h"
 #include "Types/NBT.h"
 
-#define BYTE Uint8
-
 /**
 * @file FileLoading.hpp
 * @brief The header file for the file handler of the TtEL Minecraft Alpha Level Mapper
@@ -21,12 +19,30 @@
  * @brief A class representing the level.dat file
  */
 class LEVEL_DATA {
+public:
+	/**
+	 * @brief Loads a level.dat file
+	 * @param path - Filepath pointing to a level.dat file to load from
+	 * @return True on success, or false on failure
+	 */
+	bool loadFile(std::string path);
 
+	/**
+	 * @brief Closes the file stream and frees memory
+	 */
+	void closeFile(void);
 protected:
-	// the file stream context
-	SDL_RWops* RWops;
+	// the input file stream context
+	SDL_RWops* RWops = NULL;
+	// the output file stream context
+	SDL_RWops* out_RWops = NULL;
 	// the Gzip compressed data in the file
 	std::string compressedData;
 	// the seed of the world
 	LongTag seed;
+};
+
+class CHUNK_DATA {
+protected:
+	LongTag lastUpdate;
 };
